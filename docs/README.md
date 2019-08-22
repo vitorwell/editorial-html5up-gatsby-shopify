86d3e655e5bbc210165259ae2f3f7be63cef95c5
Carlos Guimaraes <sekmet@open4g.com>
2019-08-22 04:17:10 -0300

    Initial commit

---
+++ docs/README.md
@@ -0,0 +1,43 @@
+<!-- AUTO-GENERATED-CONTENT:START (STARTER) -->
+<h1 align="center">
+   🏪 Editorial by HTML5 UP + Gatsby + Shopify + Netlify CMS Starter
+</h1>
+<p align="center">
+  <a href="https://github.com/sekmet/editorial-html5up-gatsby-shopify">
+    <img alt="Gatsby" src="https://github.com/sekmet/editorial-html5up-gatsby-shopify/raw/master/docs/img/tag-html5up-net-uploads-demos-editorial-2019-08-20-22_50_03.jpg" />
+  </a>
+</p>
+
+
+Kick off your project with this default boilerplate. This starter ships with the main Gatsby configuration files you might need to get up and running blazing fast with the blazing fast app generator for React.
+
+_Have another more specific idea? You may want to check out our vibrant collection of [official and community-created starters](https://www.gatsbyjs.org/docs/gatsby-starters/)._
+
+# 🚀 Quick start
+
+<!-- Markdown snippet -->
+[![Deploy to Netlify](https://www.netlify.com/img/deploy/button.svg)](https://app.netlify.com/start/deploy?repository=https://github.com/sekmet/editorial-html5up-gatsby-shopify)
+
+# 💡 Features
+
+- in progress
+
+# 🌐 Community
+
+Netlify CMS has a [Gitter community](https://gitter.im/netlify/netlifycms) where members of the community hang out and share things about the project, as well as give and receive support.
+
+# 💎 Contributing
+
+New contributors are always welcome!
+
+# 💗 If you like, consider a donation!
+
+Every single donation is important. Your donation is gonna make a clear statement: `My work is Valued`.
+
+Your donation is going to help me devote more time to open source. I'd love to be able to devote several hours per week to maintaining existing projects as well as creating new ones.
+
+#### 🙏 Thank you for your support! 🙏
+
+<a href="https://www.patreon.com/bePatron?u=23459686" data-patreon-widget-type="become-patron-button"><img src="https://c5.patreon.com/external/logo/become_a_patron_button.png" alt="Become a Patron!" /></a>
+
+<!-- AUTO-GENERATED-CONTENT:END -->
---
+++ gatsby-config.js
@@ -0,0 +1,150 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`
+})
+//const queries = require("./src/components/searchbox/queries")
+const siteMetadata = require("./static/admin/site_metadata")
+const shopOptions = require('./gatsby-shopifystore')
+
+module.exports = {
+  siteMetadata: siteMetadata,
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/components/`,
+        name: 'components',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/pages/`,
+        name: 'pages',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/content/posts/`,
+        name: 'posts',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/static/assets/`,
+        name: 'assets',
+      },
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 750,
+              linkImagesToOriginal: false,
+              wrapperStyle: 'margin-bottom: 1.0725rem;',
+            },
+          },
+          {
+            resolve: 'gatsby-remark-responsive-iframe',
+            options: {
+              wrapperStyle: 'margin-bottom: 1.0725rem',
+            },
+          },
+          'gatsby-remark-prismjs',
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-smartypants',
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-layout`,
+    `gatsby-plugin-sass`,
+    'gatsby-plugin-catch-links',
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-twitter',
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        description: 'Gatsby starter for a blog or store',
+        homepage_url: 'https://editorial-html5up-gatsby.netlify.com',
+        start_url: '/',
+        background_color: '#fff',
+        theme_color: '#673ab7',
+        display: 'standalone',
+        crossOrigin: `use-credentials`,
+        icons: [
+          {
+            src: '/img/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/img/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    },
+    // To learn more, visit: https://gatsby.dev/offline
+    {
+      resolve: `gatsby-plugin-offline`,
+      options: {
+        cacheId: `editorial-theme-offline`
+      }
+    },
+    {
+      resolve: "gatsby-theme-shopifystore",
+        options: {
+            basePath: "/products",
+            productPath: "/product",
+            shopTagline: "My Gatsby + Shopify Store",
+            shopBtnlabel: "Learn more",
+            shopCurrency: "USD",
+            productOptions: shopOptions,
+            verbose: true,
+        }
+      },
+    /*{
+      resolve: `gatsby-plugin-algolia`,
+      options: {
+        appId: process.env.ALGOLIA_APP_ID,
+        apiKey: process.env.ALGOLIA_ADMIN_KEY,
+        //indexName: process.env.ALGOLIA_INDEX_NAME,
+        queries,
+        chunkSize: 10000, // default: 1000
+      },
+    },*/
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: '',
+      },
+    },
+    {
+      resolve: "gatsby-plugin-netlify-cms",
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-netlify',
+      options: {
+        mergeSecurityHeaders: true,
+        mergeLinkHeaders: true,
+        mergeCachingHeaders: true,
+      },
+    },
+    `gatsby-plugin-netlify-cache`
+  ],
+}
